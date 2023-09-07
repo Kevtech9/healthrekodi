49b1bce4ebc5fd7db8df211cd9060cc8a21d22fa
kibet kevin <kevinkibet9@gmail.com>
2023-09-07 14:53:30 +0300

    b
    b

---
+++ js/auth.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBhzF-yVMCbBO_IVkQ07s5iYy_QvUJIEAc",
+    authDomain: "afya1-9c435.firebaseapp.com",
+    databaseURL: "https://afya1-9c435-default-rtdb.firebaseio.com",
+    projectId: "afya1-9c435",
+    storageBucket: "afya1-9c435.appspot.com",
+    messagingSenderId: "752849222724",
+    appId: "1:752849222724:web:fe3d57b1c0be97d585d011"
+};
