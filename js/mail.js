0de55d0fa2468644edf165483e9752607da08b00
kibet kevin <kevinkibet9@gmail.com>
2023-09-07 13:52:41 +0300

    f.c
    f.c

---
+++ js/mail.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCWy9z34eslq54_eRuGHi9jfdbveMMLQFk",
+    authDomain: "afyarecord-forms.firebaseapp.com",
+    projectId: "afyarecord-forms",
+    storageBucket: "afyarecord-forms.appspot.com",
+    messagingSenderId: "259838909266",
+    appId: "1:259838909266:web:f9fba0d9c9415d5fc9b88b"
+};
